fd2cbbcbba302b74afd72ffe7b703502e828dde9
Gopi <GopiR5@yahoo.com>
2024-06-16 18:19:49 +0530

    react-day18-contextapi

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-3 bg-secondary d-flex justify-content-center'>
+       
+    </div>
+  )
+}
+
+export default Footer
